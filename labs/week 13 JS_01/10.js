cbe3c47a4632c5fcdc38949bd83bab2dfd0e7e54
Mohammad Alharbi <msalharbi@gmail.com>
2025-04-30 05:44:38 +0300

    js01 lect added

---
+++ labs/week 13 JS_01/10.js
@@ -0,0 +1,10 @@
+function calculateAge(birthYear) {
+    let currentYear = 2025;
+    let age = currentYear - birthYear;
+    return age;
+}
+
+let myAge = calculateAge(2000);
+  
+console.log("عمري هو: " + myAge + " سنة");
+  
